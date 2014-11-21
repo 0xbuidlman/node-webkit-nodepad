abeada05448ae2ec3d593265b0398cb507175c76
Denis Malinochkin <dmalinochkin@land.ru>
2014-11-21 23:49:33 +0300

    Local Storage

--- app/scripts/editor.js
+++ app/scripts/editor.js
@@ -1,5 +1,28 @@
-var markdown = require('markdown').markdown,
+var ls       = global.localStorage,
+    markdown = require('markdown').markdown,
     $        = global.jQuery;
+
+
+//
+// Loaded
+//
+mainWindow.on("loaded", function() {
+  var content = ls.getItem('content');
+
+  if (content) {
+    $('#editor-input').val( content );
+  }
+
+  $('#editor-input').trigger('textchange');
+});
+
+//
+// Save content to Local Storage
+//
+$('#editor-input').on("textchange", function() {
+  var content = $( this ).val();
+  ls.setItem("content", content);
+});
 
 //
 // Render when data changes
