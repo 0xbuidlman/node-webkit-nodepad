b5f77583d819990909006d62657e6be00857f9fa
Denis Malinochkin <dmalinochkin@land.ru>
2014-11-27 00:03:08 +0300

    fix error

--- app/scripts/windowEvents.js
+++ app/scripts/windowEvents.js
@@ -20,11 +20,12 @@ $('#window-resize').on("click", function() {
   if ($( this ).hasClass('maximize')) {
     mainWindow.toggleFullscreen();
     $( this ).toggleClass('maximize');
-    $( this ).children('i').toggleClass('icon-toolbar-maximize icon-toolbar-minimize');
   } else {
     mainWindow.toggleFullscreen();
     $( this ).toggleClass('maximize');
   };
+
+  $( this ).children('i').toggleClass('icon-toolbar-maximize icon-toolbar-minimize');
 });
 
 //
